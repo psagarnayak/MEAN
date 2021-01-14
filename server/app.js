bce9ecb3b2cfb5327e8bdfdffc9770904e85eeea
Sagar Nayak <p.sagar.nayak@gmail.com>
2021-01-14 13:49:13 +0530

    Added CORS AccessControl headers to support cross origin

--- server/app.js
+++ server/app.js
@@ -1,10 +1,16 @@
 const app = require('express')();
-const cors = require('cors')();
 const bodyParser = require('body-parser');
 const postController = require('./controllers/post.controller')
 
-app.use(cors);
 app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', '*');
+    res.setHeader('Access-Control-Allow-Methods', '*');
+    next();
+});
 
 app.use('/api/posts', postController);
 
