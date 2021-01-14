1b913879833664ea0454eb9c6c0918825fcdad9c
Sagar Nayak <p.sagar.nayak@gmail.com>
2021-01-15 02:21:20 +0530

    Added AuthGuard to protect routes that need login

--- web/src/app/app.routing.module.ts
+++ web/src/app/app.routing.module.ts
@@ -4,16 +4,18 @@ import { LoginComponent } from "./auth/login/login.component";
 import { SignupComponent } from "./auth/signup/signup.component";
 import { CreateEditPostComponent } from "./post/create-edit-post/create-edit-post.component";
 import { ShowPostsComponent } from "./post/show-posts/show-posts.component";
+import { AuthGuard } from './auth/auth.guard';
 
 const appRoutes: Routes = [
   { path: '', component: ShowPostsComponent },
-  { path: 'createPost', component: CreateEditPostComponent },
+  { path: 'createPost', component: CreateEditPostComponent, canActivate: [AuthGuard] },
   { path: 'login', component: LoginComponent },
   { path: 'signup', component: SignupComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(appRoutes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [AuthGuard]
 })
 export class AppRoutingModule { }
---
+++ web/src/app/auth/auth.guard.ts
@@ -0,0 +1,19 @@
+import { Injectable } from "@angular/core";
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from "@angular/router";
+import { Observable } from "rxjs";
+import { AuthService } from './../service/auth.service';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+
+  constructor(private authService: AuthService, private router: Router) { }
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
+
+    let isLoggedIn = this.authService.checkIfLoggedIn();
+    if (!isLoggedIn) {
+      this.router.navigate(['/login']);
+    }
+    return isLoggedIn;
+  }
+}
